4403b400b9c0eee83cd8eac3043a37bc87029d6d
jeffrey arbelaez <jeffreyarbelaez@yahoo.com>
2025-05-29 23:51:44 -0500

    some of part 2

---
+++ controllers/petController.js
@@ -0,0 +1,13 @@
+const petsData = require ("..data/pets.js")
+
+const getAllPets = async (req, resizeBy, next) => {
+
+    try{
+        const pets = petsData
+        return response.status (200)
+    }
+
+
+
+
+}
